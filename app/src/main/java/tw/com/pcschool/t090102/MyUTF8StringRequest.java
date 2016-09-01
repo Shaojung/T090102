5b5e137525cce5885234db0cbe20247abfb765a1
Shaojung <shaojung@gmail.com>
2016-09-01 21:28:38 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/t090102/MyUTF8StringRequest.java
@@ -0,0 +1,35 @@
+package tw.com.pcschool.t090102;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by teacher on 2016/9/1.
+ */
+public class MyUTF8StringRequest extends StringRequest {
+    public MyUTF8StringRequest(int method, String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        super(method, url, listener, errorListener);
+    }
+    public MyUTF8StringRequest(String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        super(url, listener, errorListener);
+    }
+
+    @Override
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+
+        String utf8String = null;
+        try {
+            utf8String = new String(response.data, "UTF-8");
+            return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        }
+    }
+
+}
